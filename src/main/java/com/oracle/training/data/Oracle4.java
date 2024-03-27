f1ffc106c2b848dc4ddd3b730e52bc844f9aca6b
tniita <takuya.niita@oracle.com>
2024-03-27 23:08:29 +0900

    update

--- src/main/java/com/oracle/training/data/Oracle4.java
+++ src/main/java/com/oracle/training/data/Oracle4.java
@@ -2,15 +2,12 @@ package com.oracle.training.data;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Builder
 @Getter
+@ToString
 public class Oracle4 {
-    @Override
-    public String toString() {
-        return "Oracle4 [employeeNum=" + employeeNum + ", age=" + age + ", name=" + name + ", department=" + department
-                + "]";
-    }
     private final int employeeNum;
     private final int age;
     private final String name;
